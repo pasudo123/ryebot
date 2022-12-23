d4628a80bad0b7588e7ac861875b12ebe4238127
PARK SUNG DONG <oraedoa@gmail.com>
2022-12-24 00:56:35 +0900

    study

--- src/main/kotlin/com/github/ryebot/api/WebHookController.kt
+++ src/main/kotlin/com/github/ryebot/api/WebHookController.kt
@@ -2,16 +2,14 @@ package com.github.ryebot.api
 
 import com.github.ryebot.api.model.TriggerRequest
 import com.github.ryebot.application.WebHookService
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("trigger")
 class WebHookController(
     private val webHookService: WebHookService
 ) {
+
 
     @PostMapping
     fun trigger(@RequestBody triggerRequest: TriggerRequest) {
